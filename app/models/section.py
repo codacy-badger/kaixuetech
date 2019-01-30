7878d6baa016c1facaed76ac7fc0adbc4d3e8d84
kaixue <903165495@qq.com>
2019-01-30 22:52:38 +0800

    章节的数据库

--- app/models/section.py
+++ app/models/section.py
@@ -3,16 +3,14 @@
 # @Author  : 昨夜
 # @Email   : 903165495@qq.com
 
-from sqlalchemy import Column, String, Integer, Text, orm, SmallInteger
+from sqlalchemy import Column, String, Integer, Text, orm, SmallInteger, DateTime,Float
 from app.models.base import Base, db
 
 """
 章节id
 章节名
-上课日期
 上课时间
 上课时长
-章节介绍
 课程资料
 章节介绍
 """
@@ -20,3 +18,7 @@ class Section(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
     subject_id= Column(Integer)
+    class_time=Column(DateTime)
+    duration=Column(Float)
+    learn_data=Column(Text)
+    abstract = Column(Text)
